8d81342698665d3db539e08a4e3398bafdf59032
Marcel Sebbin <marcelsebbin@aol.de>
2024-12-02 10:02:50 +0100

    add missing error handling

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
@@ -11,5 +12,8 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe-Fehler: ", err)
+	}
 }
